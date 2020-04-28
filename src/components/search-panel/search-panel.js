0443bb9a33d0751efa784b96987189bf9d43efb9
Ivan <ivan.s.palatov@gmail.com>
2020-04-28 19:57:10 +0400

    start

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './search-panel.css'
+
+
+const SearchPanel = () => {
+    return (
+        <input className='form-control search-input' type='text' placeholder='поиск по записям'></input>
+    )
+}
+
+export default SearchPanel;
