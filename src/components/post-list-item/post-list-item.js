57a518db941299efa80d491426c24b47226799b9
Ivan <ivan.s.palatov@gmail.com>
2020-04-30 17:27:59 +0400

    add likes and important marks, filter button and search input. All dynamically

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -4,31 +4,9 @@ import './post-list-item.css';
 
 
 export default class PostListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            important: false,
-            like: false
-        }
-        this.OnImportant = this.OnImportant.bind(this);
-        this.OnLike = this.OnLike.bind(this);
-    }
-    OnImportant() {
-        this.setState(({important}) => ({
-            important: !important
-        }))
-    }
-
-    OnLike() {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
-
-
+   
     render() {
-        const { label, onDelete } = this.props;
-        const { important, like } = this.state;
+        const { label, onDelete, onToggleImportant, onToggleLiked, important, like } = this.props;
         let classNames = 'app-list-item d-flex justify-content-between';
         if (important) {
             classNames += ' important';
@@ -40,14 +18,14 @@ export default class PostListItem extends Component {
             <div className={classNames}>
                 <span 
                 className='app-list-item-label'
-                onClick={this.OnLike} >
+                onClick={onToggleLiked} >
                     {label}
                 </span>
                 <div className="d-flex justify-content-center align-items-center">
                     <button
                         type='button'
                         className='btn-star btn-sm'
-                        onClick={this.OnImportant} >
+                        onClick={onToggleImportant} >
                         <i className='fa fa-star'></i>
                     </button>
                     <button
